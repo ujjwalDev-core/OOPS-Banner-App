7542c9ce3e7a9170cb6ff8d90e935a9d43eb47ee
ujjwalDev-core <ujjwalgithub@gmail.com>
2026-03-03 08:52:33 +0530

    UC8: Implement HashMap-based banner rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
+<<<<<<< Updated upstream
     public static void main(String[] args) {
 
         String[] oPattern = getOPattern();
@@ -55,5 +59,71 @@ public class OOPSBannerApp {
                 " *     * ",
                 "  *****  "
         };
+=======
+    public static Map<Character, String[]> buildCharacterMap() {
+
+        Map<Character, String[]> bannerMap = new HashMap<>();
+
+        bannerMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        bannerMap.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        bannerMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return bannerMap;
+    }
+
+    public static void printBanner(String message,
+                                   Map<Character, String[]> bannerMap) {
+
+        int height = 7;
+
+        for (int row = 0; row < height; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (int col = 0; col < message.length(); col++) {
+
+                char ch = message.charAt(col);
+                String[] pattern = bannerMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> bannerMap = buildCharacterMap();
+        printBanner("OOPS", bannerMap);
+>>>>>>> Stashed changes
     }
 }
