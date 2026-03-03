e09858f136940b1ba0553c7c9b1430b28770128d
ujjwalDev-core <ujjwalgithub@gmail.com>
2026-03-03 08:59:10 +0530

    UC8: Implement HashMap-based banner rendering for OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,63 +3,6 @@ import java.util.Map;
 
 public class OOPSBannerApp {
 
-<<<<<<< Updated upstream
-    public static void main(String[] args) {
-
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
-
-        String[] banner = new String[7];
-
-        for (int i = 0; i < 7; i++) {
-            banner[i] = String.join(" ",
-                    oPattern[i],
-                    oPattern[i],
-                    pPattern[i],
-                    sPattern[i]);
-        }
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
-    }
-
-    public static String[] getOPattern() {
-        return new String[]{
-                "  *****  ",
-                " *     * ",
-                "*       *",
-                "*       *",
-                "*       *",
-                " *     * ",
-                "  *****  "
-        };
-    }
-
-    public static String[] getPPattern() {
-        return new String[]{
-                "  ****** ",
-                " *     * ",
-                " *     * ",
-                "  ****** ",
-                " *       ",
-                " *       ",
-                " *       "
-        };
-    }
-
-    public static String[] getSPattern() {
-        return new String[]{
-                "  *****  ",
-                " *     * ",
-                " *       ",
-                "  *****  ",
-                "        *",
-                " *     * ",
-                "  *****  "
-        };
-=======
     public static Map<Character, String[]> buildCharacterMap() {
 
         Map<Character, String[]> bannerMap = new HashMap<>();
@@ -124,6 +67,5 @@ public class OOPSBannerApp {
 
         Map<Character, String[]> bannerMap = buildCharacterMap();
         printBanner("OOPS", bannerMap);
->>>>>>> Stashed changes
     }
 }
